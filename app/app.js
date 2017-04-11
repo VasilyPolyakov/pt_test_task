e3d882abfff8c5285bb63260f2eea0f7eb37c1ef
vpv <vpv@opencascade.com>
2017-04-11 17:44:21 +0300

    Correct typos

--- app/app.js
+++ app/app.js
@@ -6,6 +6,5 @@ import routing from './app.routing';
 import basket from './components/basket/index';
 import exchange from './components/exchange/index';
 
-
 angular.module('app', [uirouter, basket, exchange])
     .config(routing);
